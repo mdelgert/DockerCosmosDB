4b6d9755a9cbe48381c6f7f66c2c55748f151edc
Matthew David Elgert <mdelgert@yahoo.com>
2026-01-05 20:31:34 -0700

    Update project dependencies and restructure services
    - Added package references for `Microsoft.Azure.Cosmos` and `Newtonsoft.Json` in `DockerCosmosDB.Backend.csproj`.
    - Updated Swagger UI link in `Home.razor` to use port `8881`.
    - Enhanced `README.md` with a new link for CosmosDB development resources.
    - Commented out old Docker service URL and name in `docker-compose.dcproj`, replacing with a new frontend service name.
    - Changed port mappings in `docker-compose.override.yml` from `8080`/`8081` to `8880`/`8881` and from `8082`/`8083` to `8882`/`8883`.
    - Added `HelloWorldController` with a simple API endpoint returning a JSON message.

---
+++ DockerCosmosDB.Backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerCosmosDB.Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class HelloWorldController : Controller
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public HelloWorldController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "HelloWorldGet")]
+    public IActionResult Get()
+    {
+        var message = new { message = "helloworld" };
+
+        return Json(message);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Demo DockerCosmosDB
+
+# Links
+https://www.johanohlin.com/posts/2019-07-05-develop-with-cosmosdb-in-docker/
