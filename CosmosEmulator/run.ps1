9a3e69f61fe35c377ef93843d1df57b9f1340d77
Matthew David Elgert <mdelgert@yahoo.com>
2026-01-07 09:02:25 -0700

    Add initial run script for Cosmos DB emulator with external access configuration

---
+++ CosmosEmulator/run.ps1
@@ -0,0 +1,8 @@
+# Start emulator with external access enabled
+cd "C:\Program Files\Azure Cosmos DB Emulator\"
+
+# .\CosmosDB.Emulator.exe /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
+
+# .\Microsoft.Azure.Cosmos.Emulator.exe /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
+
+.\Microsoft.Azure.Cosmos.Emulator.exe /EnableRateLimiting /DisableRateLimiting /PartitionCount=25 /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
