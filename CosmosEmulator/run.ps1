536bb49df0bd36a0fc6af6fe929a5aa3a81b282e
Matthew Elgert <mdelgert@yahoo.com>
2026-01-09 09:29:28 -0700

    Update partition count in Cosmos DB emulator run script to 3

--- CosmosEmulator/run.ps1
+++ CosmosEmulator/run.ps1
@@ -5,4 +5,4 @@ cd "C:\Program Files\Azure Cosmos DB Emulator\"
 
 # .\Microsoft.Azure.Cosmos.Emulator.exe /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
 
-.\Microsoft.Azure.Cosmos.Emulator.exe /EnableRateLimiting /DisableRateLimiting /PartitionCount=25 /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
+.\Microsoft.Azure.Cosmos.Emulator.exe /EnableRateLimiting /DisableRateLimiting /PartitionCount=3 /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
